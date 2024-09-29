ec0368ca35dc0fa65e88f2b2fe2a0060eac143d5
tamimhossain11 <amdtamim3@gmail.com>
2024-09-29 12:32:01 +0600

    link updates made

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://giyapay-qr.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
